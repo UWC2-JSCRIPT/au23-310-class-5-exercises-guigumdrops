6c217d04b1d6c604d1eebd7435bc12b7cb3770b4
Krish <krshrg@gmail.com>
2023-11-11 23:17:23 -0800

    save 4

--- toDoList.js
+++ toDoList.js
@@ -8,22 +8,26 @@ liElements.forEach(liElement => {
 });
 
 // If a delete link is clicked, delete the li element / remove from the DOM
-const delLink = document.getElementsByClassName('delete')
-delLink.forEach(deleteLink => {
-  delLink.addEventListener('click', function() {
+const delLinks = document.querySelectorAll('.delete');
+
+delLinks.forEach(deleteLink => {
+  deleteLink.addEventListener('click', function() {
     const liElement = this.closest('li');
     liElement.parentNode.removeChild(liElement);
-  }
-
-  )
-})
+  });
+});
 // If an 'Add' link is clicked, adds the item as a new list item with
 // addListItem function has been started to help you get going!
 // Make sure to add an event listener(s) to your new <li> (if needed)
 const addListItem = function(e) {
   e.preventDefault();
-  const input = this.parentNode.getElementsByTagName('input')[0];
+  const input = this.parentNode.querySelectorAll('input')[0];
   const text = input.value; // use this text to create a new <li>
-
-  // Finish function here
+  const listItem = document.createElement('li');
+  listItem.textContent = text;
+  const uEl = document.getElementsByClassName('today-list')[0];
+  uEl.appendChild(listItem);
+  input.value = '';
 };
+const addEl = document.getElementsByClassName('add-item')[0];
+addEl.addEventListener('click', addListItem);
